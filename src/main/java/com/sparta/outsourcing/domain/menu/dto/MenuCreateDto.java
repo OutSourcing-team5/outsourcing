fafd4c38387aea2cb91105a75337904cfa6677ac
JJH <whwndgnl12@naver.com>
2024-11-04 15:35:06 +0900

    feat:create 메뉴

--- src/main/java/com/sparta/outsourcing/domain/menu/dto/MenuCreateDto.java
+++ src/main/java/com/sparta/outsourcing/domain/menu/dto/MenuCreateDto.java
@@ -1,14 +1,16 @@
 package com.sparta.outsourcing.domain.menu.dto;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 
 @Getter
 public class MenuCreateDto {
-	@NotBlank
+	@NotBlank(message = "메뉴 이름은 필수 입력 항목입니다.")
 	private String menuName;
 
+	@Min(value = 1, message = "가격은 0보다 커야 합니다.")
 	private int price;
 
+	@NotNull(message = "storeId는 필수 입력 항목입니다.")
 	private Long storeId;
 }
