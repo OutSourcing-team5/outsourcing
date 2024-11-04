95f7481174749252ad3b02a1eee9e666ba446fcc
jangms3 <github.com/jangms3>
2024-11-04 14:15:40 +0900

    1

--- src/main/java/com/sparta/outsourcing/domain/order/repository/OrderRepository.java
+++ src/main/java/com/sparta/outsourcing/domain/order/repository/OrderRepository.java
@@ -1,8 +1,6 @@
 package com.sparta.outsourcing.domain.order.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.sparta.outsourcing.domain.order.entity.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
