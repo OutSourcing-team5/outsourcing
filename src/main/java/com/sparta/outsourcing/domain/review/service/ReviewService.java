69b4a4bd066f9ea9ee7650ea4deb6ba350466df7
Minwoo <alsdn2606@gmail.com>
2024-11-05 15:29:18 +0900

    fix: 리뷰 생성시 발생하던 오류 해결
    -Order 를 제대로 참조하지 않고 리뷰를 생성하지 못하던 에러 해결
    -리뷰 생성 및 수정, 삭제로직 테스트완료

--- src/main/java/com/sparta/outsourcing/domain/review/service/ReviewService.java
+++ src/main/java/com/sparta/outsourcing/domain/review/service/ReviewService.java
@@ -43,7 +43,8 @@ public class ReviewService {
 		if(!order.getMember().getId().equals(memberId)) {
 			throw new IllegalArgumentException("요청된 주문은 해당 멤버의 주문이 아닙니다.");
 		}
-		Review newReview = Review.createOf(requestDto.getRating());
+		Review newReview = Review.createReview(requestDto.getRating(), order);
+		reviewRepository.save(newReview);
 		return new ReviewResponseDto(newReview);
 	}
 
