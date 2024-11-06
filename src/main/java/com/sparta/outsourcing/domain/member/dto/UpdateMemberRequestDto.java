2bbff3fd21d11f9616576d2c1bf7b36b030a46b8
JJH <whwndgnl12@naver.com>
2024-11-06 12:30:54 +0900

    fix:테스트용 주석 삭제

--- src/main/java/com/sparta/outsourcing/domain/member/dto/UpdateMemberRequestDto.java
+++ src/main/java/com/sparta/outsourcing/domain/member/dto/UpdateMemberRequestDto.java
@@ -10,8 +10,8 @@ public class UpdateMemberRequestDto {
 	private String username;
 	@NotNull(message = "비밀번호를 입력하세요")
 	private String oldPassword;
-	// @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$",
-	// 	message = "숫자, 문자, 특수문자가 1개 이상 사용되어야 함, 길이: 8 ~ 16")
+	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$",
+		message = "숫자, 문자, 특수문자가 1개 이상 사용되어야 함, 길이: 8 ~ 16")
 	private String newPassword;
 	private String address;
 }
